a2bf4966779e5e215dbf520bf7a43436c43471b8
Tan <pochara.youcharoen@gmail.com>
2022-09-22 02:27:30 +0700

    fix: schema

--- models/Users.js
+++ models/Users.js
@@ -5,13 +5,15 @@ const { Schema } = mongoose;
 
 const schema = new Schema({
   twitter_id: String,
-  twitter_name: String,
+  display_name: String,
+  description: String,
   wallet_address: String,
   nft_collections: Schema.Types.Array,
   profile_picture: String,
   position: Schema.Types.Point,
   isOnline: Boolean,
   message: String,
+  tags: Schema.Types.Array
 });
 
 schema.index({ position: `2dsphere` });
